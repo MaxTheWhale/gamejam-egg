dac1b7779032bb070a62ce7c6734a8378fc1e455
Jonny <jonathan.hall859@gmail.com>
2019-11-22 21:25:28 +0000

    Added boing

---
+++ Assets/MoveThing.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[BoltGlobalBehaviour(BoltNetworkModes.Server)]
+public class MoveThing : MonoBehaviour
+{
+    public float force = 600;
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collider hit");
+        Rigidbody hit = collision.collider.gameObject.GetComponent<Rigidbody>();
+        if (hit != null)
+        {
+            Debug.Log(hit);
+            hit.AddForce(new Vector3(0, force, 0));
+        }
+    }
+}
