59ae190323e1bc826270804e02a29ef88aebd99a
Jonny <jonathan.hall859@gmail.com>
2019-11-22 14:41:04 +0000

    Fixed egg controller script

--- Assets/Scripts/NetworkCallbacks.cs
+++ Assets/Scripts/NetworkCallbacks.cs
@@ -8,9 +8,12 @@ public class Network : Bolt.GlobalEventListener
     public override void SceneLoadLocalDone(string scene)
     {
         Vector3 spawnPos = new Vector3(Random.Range(-4, 4), 0, Random.Range(-4, 4));
+        
         BoltEntity egh = BoltNetwork.Instantiate(BoltPrefabs.Egg, spawnPos, Quaternion.identity);
         GameObject camera = GameObject.FindGameObjectWithTag("MainCamera");
+
         camera.GetComponent<CameraController>().target = egh.gameObject.transform;
+        egh.GetComponent<EggController>().followCam = camera.transform;
         base.SceneLoadLocalDone(scene);
     }
 }
