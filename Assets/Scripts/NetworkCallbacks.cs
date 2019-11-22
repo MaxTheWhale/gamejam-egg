7a600db3809ec48ec0ed4aa05f99a1e24d1ba268
Jonny <jonathan.hall859@gmail.com>
2019-11-22 14:32:02 +0000

    No more fall through world

--- Assets/Scripts/NetworkCallbacks.cs
+++ Assets/Scripts/NetworkCallbacks.cs
@@ -7,7 +7,7 @@ public class Network : Bolt.GlobalEventListener
 {
     public override void SceneLoadLocalDone(string scene)
     {
-        Vector3 spawnPos = new Vector3(Random.Range(-4, 4), Random.Range(-4, 4));
+        Vector3 spawnPos = new Vector3(Random.Range(-4, 4), 0, Random.Range(-4, 4));
         BoltEntity egh = BoltNetwork.Instantiate(BoltPrefabs.Egg, spawnPos, Quaternion.identity);
         GameObject camera = GameObject.FindGameObjectWithTag("MainCamera");
         camera.GetComponent<CameraController>().target = egh.gameObject.transform;
