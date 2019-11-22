0fa3cacf05dd73e350996d0484004db06286d645
Jonny <jonathan.hall859@gmail.com>
2019-11-22 14:02:59 +0000

    MULTIPLAYOR

---
+++ Assets/Scripts/NetworkCallbacks.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[BoltGlobalBehaviour]
+public class Network : Bolt.GlobalEventListener
+{
+    public override void SceneLoadLocalDone(string scene)
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-4, 4), Random.Range(-4, 4));
+        BoltNetwork.Instantiate(BoltPrefabs.Egg, spawnPos, Quaternion.identity);
+        base.SceneLoadLocalDone(scene);
+    }
+}
