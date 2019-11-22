4b72eb14bec3b998d8b94ea9190adcd4fd33eed4
Jonny <jonathan.hall859@gmail.com>
2019-11-22 16:36:32 +0000

    CONTROLLLLLLERR

--- Assets/Scripts/EggController.cs
+++ Assets/Scripts/EggController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Bolt;
 using UnityEngine;
 
 public class EggController : Bolt.EntityBehaviour<IEggState>
@@ -16,25 +17,54 @@ public class EggController : Bolt.EntityBehaviour<IEggState>
 
     public override void SimulateOwner()
     {
+        IEggMoveInput input = EggMove.Create();
+        if (Input.GetKey(KeyCode.W))
+        {
+            input.Forward = true;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            input.Left = true;
+            //rb.AddForce(-right * thrust);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            input.Backward = true;
+            //rb.AddForce(-forward * thrust);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            input.Right = true;
+            //rb.AddForce(right * thrust);
+        }
+        entity.QueueInput(input);
+    }
+
+    public override void ExecuteCommand(Command command, bool resetState)
+    {
         Vector3 forward = new Vector3(followCam.forward.x, 0, followCam.forward.z);
         forward.Normalize();
         Vector3 right = new Vector3(followCam.right.x, 0, followCam.right.z);
         right.Normalize();
-        if (Input.GetKey(KeyCode.W))
+
+        EggMove cmd = (EggMove)command;
+        if (cmd.Input.Forward)
         {
             rb.AddForce(forward * thrust);
         }
-        if (Input.GetKey(KeyCode.A))
+        if (cmd.Input.Left)
         {
             rb.AddForce(-right * thrust);
         }
-        if (Input.GetKey(KeyCode.S))
+        if (cmd.Input.Backward)
         {
             rb.AddForce(-forward * thrust);
         }
-        if (Input.GetKey(KeyCode.D))
+        if (cmd.Input.Right)
         {
             rb.AddForce(right * thrust);
         }
+        cmd.Result.Position = transform.position;
+        base.ExecuteCommand(command, resetState);
     }
 }
