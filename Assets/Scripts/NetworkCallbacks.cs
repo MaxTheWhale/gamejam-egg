e953caa4b38c2bd173b28ab79739a32a5143257e
Jonny <jonathan.hall859@gmail.com>
2019-11-22 14:26:13 +0000

    EEET VERKS

--- Assets/Scripts/NetworkCallbacks.cs
+++ Assets/Scripts/NetworkCallbacks.cs
@@ -8,7 +8,9 @@ public class Network : Bolt.GlobalEventListener
     public override void SceneLoadLocalDone(string scene)
     {
         Vector3 spawnPos = new Vector3(Random.Range(-4, 4), Random.Range(-4, 4));
-        BoltNetwork.Instantiate(BoltPrefabs.Egg, spawnPos, Quaternion.identity);
+        BoltEntity egh = BoltNetwork.Instantiate(BoltPrefabs.Egg, spawnPos, Quaternion.identity);
+        GameObject camera = GameObject.FindGameObjectWithTag("MainCamera");
+        camera.GetComponent<CameraController>().target = egh.gameObject.transform;
         base.SceneLoadLocalDone(scene);
     }
 }
