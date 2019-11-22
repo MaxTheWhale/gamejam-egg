27cbbdf106ac2d47ea408931cc8043eb20921b5b
Max Whale <maxwhale99@gmail.com>
2019-11-22 13:17:46 +0000

    EGG MOVE

---
+++ Assets/Scripts/EggController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EggController : MonoBehaviour
+{
+    public float thrust;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rb.AddForce(Vector3.forward * thrust);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            rb.AddForce(-Vector3.right * thrust);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            rb.AddForce(-Vector3.forward * thrust);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            rb.AddForce(Vector3.right * thrust);
+        }
+    }
+}
